4048143fd0bdb3fe8a90b1fd6c560a5bace2f458
itasouza <itasouza@yahoo.com.br>
2019-10-23 00:12:01 -0300

    Criação do banco e configuração de newsletter

---
+++ LojaVirtual/LojaVirtual/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using LojaVirtual.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        DbSet<Cliente> Clientes { get; set; }
+        DbSet<NewsLetterEmail> NewsLetterEmails { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
